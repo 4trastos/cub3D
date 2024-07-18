3de42152b4c36669639be791c39a9d3149ccff65
4trastos <93524280+4trastos@users.noreply.github.com>
2024-07-18 13:28:24 +0200

    Parse OK al 90%

--- src/brain.c
+++ src/brain.c
@@ -6,104 +6,18 @@
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 08:30:35 by davgalle          #+#    #+#             */
-/*   Updated: 2024/07/17 18:38:36 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:31:13 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/cube3d.h"
 
-void	wanted_road(t_brain *brain, char **map, int *y, int *x)
+void	reset_move(t_brain *brain)
 {
-	int	len;
-	int	alt;
-	int	aux;
-
-	len = ft_strlencust(map[*y]) - 1;
-	alt = ft_mtx_len(map) - 1;
-	aux = 0;
-	reset_move(brain);
-	if (*y == 0)
-	{
-		printf("XXXXXXXXXXXXXXX   PRIMERA LINEA   XXXXXXXXXXXXXXXXXXXXX\n\n");
-		aux = ft_strlencust(map[*y + 1]) - 1;
-		if (*x == len && map[*y + 1][*x] == '1')
-			brain->down = true;
-		else if (*x <= aux)
-		{
-			if (*x < aux && map[*y][*x + 1] == '1')
-				brain->right = true;
-			else if ((*x == aux) && map[*y + 1][*x] == '1')
-				brain->down = true;
-		}
-	}
-	else if (*y > 0 && *y < alt)
-	{
-		printf("XXXXXXXXXXXXXXX  LINEAS INTERMEDIAS   XXXXXXXXXXXXXXXXXXXXX\n\n");
-		aux = ft_strlencust(map[*y + 1]) - 1;
-		if (*x == len)
-		{
-			if (len > aux && map[*y][*x - 1] == '1')
-				brain->left = true;
-			else if (map[*y + 1][*x] == '1')
-				brain->down = true;
-		}
-		else if (map[*y - 1][*x] == 'F' &&
-			(*y - 1 == brain->init_y && *x == brain->init_x))
-			brain->up = true;
-		// else if (map[*y][*x + 1] == '1' && map[*y + 1][*x + 1] == ' ')
-		// 	brain->down = true;
-		else if (map[*y][*x + 1] == '1' && map[*y - 1][*x + 1] == ' ')
-			brain->right = true;
-		else if (map[*y][*x + 1] == '1' && map[*y - 1][*x + 1] == 'F')
-		{
-			if (*y - 1 == brain->init_y && *x + 1 == brain->init_x)
-				brain->right = true;
-		}
-		else if (len >= aux)
-		{
-			// if (*x < aux && map[*y + 1][*x] == '1')
-			// 	brain->down = true;
-			if (*x < aux && *x > 0)
-			{
-				if (map[*y + 1][*x] == '1')
-					brain->down = true;
-				else if (map[*y][*x - 1] == '1')
-					brain->left = true;
-			}
-			else if ((*x == aux) && map[*y + 1][*x] == '1')
-				brain->down = true;
-			else if (map[*y - 1][*x] == '1')
-				brain->up = true;
-			else if (*x > 0 && map[*y][*x - 1] == '1')
-				brain->left = true;
-		}
-		// else if (map[*y + 1][*x] == '1')
-		// 	brain->down = true;
-		// else if (map[*y - 1][*x] == '1')
-		// 	brain->up = true;
-		// else if (*x > 0 && map[*y][*x - 1] == '1')
-		// 	brain->left = true;
-	}
-	else if (*y == alt)
-	{
-		printf("XXXXXXXXXXXXXXX   ULTIMA LINEA   XXXXXXXXXXXXXXXXXXXXX\n\n");
-		if (*x == len && map[*y][*x - 1] == '1')
-			brain->left = true;
-		else if (*x > 0 && map[*y][*x - 1] == '1')
-			brain->left = true;
-		else if (*x < len && map[*y - 1][*x] == '1')
-			brain->up = true;
-	}
-}
-
-void	skip_space(char *str, int *x)
-{
-	int		i;
-
-	i = 0;
-	while (str[i] == ' ' && str[i] != '\0')
-		i++;
-	*x += i;
+	brain->up = false;
+	brain->down = false;
+	brain->left = false;
+	brain->right = false;
 }
 
 void	save_init(int x, int y, t_brain *brain)
@@ -113,52 +27,45 @@ void	save_init(int x, int y, t_brain *brain)
 	brain->drowned = false;
 }
 
+void	wanted_road(t_brain *brain, char **map, int *y, int *x)
+{
+	int	alt;
+
+	alt = ft_mtx_len(map) - 1;
+	reset_move(brain);
+	if (*y == 0)
+		first_line(brain, map, y, x);
+	else if (*y > 0 && *y < alt)
+		intermediate_lines(brain, map, y, x);
+	else if (*y == alt)
+		last_line(brain, map, y, x);
+}
+
 void	walking_border(t_brain *brain, char **map, int *y, int *x)
 {
-	char	c;
-
 	wanted_road(brain, map, y, x);
-	c = 'F';
 	if (brain->right)
 	{
-		printf("POSICIÓN X:   %d ---->>>>  %c  --->>>  %s\n", *x, map[*y][*x], map[*y]);
-		map[*y][*x] = c;
-		printf("CAMBIA LETRA:   ---------------->>>  %s\n", map[*y]);
+		map[*y][*x] = 'F';
 		(*x)++;
-		printf("\n");
 	}
 	else if (brain->down)
 	{
-		printf("POSICIÓN X:   %d ---->>>>  %c  --->>>  %s\n", *x, map[*y][*x], map[*y]);
-		map[*y][*x] = c;
-		printf("CAMBIA LETRA:   ---------------->>>  %s\n", map[*y]);
+		map[*y][*x] = 'F';
 		(*y)++;
-		printf("\n");
-		printf("        + + + + + + + BAJA LINEA + + + + + + +\n\n");
-
 	}
 	else if (brain->left)
 	{
-		printf("POSICIÓN X:   %d ---->>>>  %c  --->>>  %s\n", *x, map[*y][*x], map[*y]);
-		map[*y][*x] = c;
-		printf("CAMBIA LETRA:   ---------------->>>  %s\n", map[*y]);
+		map[*y][*x] = 'F';
 		(*x)--;
-		printf("\n");
 	}
 	else if (brain->up)
 	{
-		printf("POSICIÓN X:   %d ---->>>>  %c  --->>>  %s\n", *x, map[*y][*x], map[*y]);
-		map[*y][*x] = c;
-		printf("CAMBIA LETRA:   ---------------->>>  %s\n", map[*y]);
+		map[*y][*x] = 'F';
 		(*y)--;
-		printf("\n");
-		printf("        + + + + + + + SUBE LINEA + + + + + + +\n\n");
 	}
 	else
-	{
 		brain->drowned = true;
-		printf("################ NO SE PUEDE MOVER ##############\n");
-	}
 }
 
 int	walking_the_wall(char **map, t_brain *brain)
@@ -174,51 +81,9 @@ int	walking_the_wall(char **map, t_brain *brain)
 		save_init(x, y, brain);
 	while (map[y] != NULL && brain->drowned == false)
 		walking_border(brain, map, &y, &x);
+	ft_printmap(map);
 	if (y == brain->init_y && x == brain->init_x)
 		return (1);
 	return (0);
 }
 
-/* void	wanted_road(t_brain *brain, char **map, int *y, int *x)
-{
-	int	len;
-	int	alt;
-
-	len = ft_strlencust(map[*y]) - 1;
-	alt = ft_mtx_len(map) -1;
-	reset_move(brain);
-	if (*y == 0)
-	{
-		if (*x == 0)
-		{
-			if (x == len && map[*y++][*x] == '1')
-				brain->down = true;
-			else if (map[*y][*x++] == '1' && map[*y++][*x++] == ' '
-				&& map[*y++][*x] == '1')
-				brain->down = true;
-			else if (map[*y][*x++] == '1' && map[*y++][*x++] != ' ')
-				brain->right = true;
-		}
-		else if (*x > 0 && *x < len)
-		{
-			if (map[*y][*x++] == '1' && map[*y++][*x++] != ' ')
-				brain->right = true;
-			else if (map[*y][*x++] == '1' && map[*y++][*x++] == ' '
-				&& map[*y++][*x] == '1')
-				brain->down = true;
-		}
-		else if (*x == len && map[*y++][*x] == '1')
-			brain->down = true;
-	}
-	else if (*y > 0 && *y < alt)
-	{
-		if (map[*y--][*x] == ' ' && map[*y][*x++] == '1')
-			brain->right = true;
-		else if (map[*y--][*x] == '1' && map[*y--][])
-			brain->up = true;
-	}
-	else if (*y == alt)
-	{
-		
-	}
-} */
--- src/brain_2.c
+++ src/brain_2.c
@@ -6,16 +6,104 @@
 /*   By: davgalle <davgalle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 08:30:35 by davgalle          #+#    #+#             */
-/*   Updated: 2024/07/17 09:32:39 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:50:51 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/cube3d.h"
 
-void	reset_move(t_brain *brain)
+void	first_line(t_brain *brain, char **map, int *y, int *x)
 {
-	brain->up = false;
-	brain->down = false;
-	brain->left = false;
-	brain->right = false;
+	int	aux;
+	int	len;
+
+	len = ft_strlencust(map[*y]) - 1;
+	aux = ft_strlencust(map[*y + 1]) - 1;
+	if (*x == len && map[*y + 1][*x] == '1')
+		brain->down = true;
+	else if (*x <= aux)
+	{
+		if (*x < aux && map[*y][*x + 1] == '1')
+			brain->right = true;
+		else if ((*x == aux) && map[*y + 1][*x] == '1')
+			brain->down = true;
+	}
+}
+
+void	last_line(t_brain *brain, char **map, int *y, int *x)
+{
+	int	len;
+
+	len = ft_strlencust(map[*y]) - 1;
+	if (*x == len && map[*y][*x - 1] == '1')
+		brain->left = true;
+	else if (*x > 0 && map[*y][*x - 1] == '1')
+		brain->left = true;
+	else if (*x < len && map[*y - 1][*x] == '1')
+		brain->up = true;
+}
+
+void	intermediate_lines(t_brain *brain, char **map, int *y, int *x)
+{
+	int	aux;
+	int	len;
+
+	len = ft_strlencust(map[*y]) - 1;
+	aux = ft_strlencust(map[*y + 1]) - 1;
+	printf("************** LINEAS INTERMEDIAS ********\n");
+	printf("LINEA:  %d    FILA:   %d\n", *y, *x);
+	if (*x == len)
+	{
+		if (len > aux && map[*y][*x - 1] == '1')
+			brain->left = true;
+		else if (map[*y + 1][*x] == '1')
+			brain->down = true;
+	}
+	else if (map[*y - 1][*x] == 'F' &&
+		(*y - 1 == brain->init_y && *x == brain->init_x))
+		brain->up = true;
+	else if (map[*y][*x + 1] == '1' && map[*y - 1][*x + 1] == ' ')
+		brain->right = true;
+	else if (map[*y - 1][*x] == ' ' && map[*y][*x + 1] == '1' &&
+		map[*y - 1][*x + 1] == '1')
+		brain->right = true;
+	else if (map[*y][*x + 1] == '1' && map[*y - 1][*x + 1] == 'F')
+	{
+		if (*y - 1 == brain->init_y && *x + 1 == brain->init_x)
+			brain->right = true;
+	}
+	else if (len >= aux)
+	{
+		if (*x < aux && *x > 0)
+		{
+			if (map[*y + 1][*x] == '1')
+				brain->down = true;
+			else if (map[*y][*x - 1] == ' ' && map[*y - 1][*x] == '1')
+				brain->up = true;
+			else if (map[*y][*x - 1] == '1')
+				brain->left = true;
+			else if (map[*y - 1][*x] == '1' && map[*y - 1][*x - 1] == ' ')
+				brain->up = true;
+		}
+		else if (*x == aux && *x > 0)
+		{
+			if (map[*y + 1][*x] == '1')
+				brain->down = true;
+			else if (map[*y - 1][*x] == '1')
+				brain->up = true;
+			else if (map[*y][*x - 1] == '1')
+				brain->left = true;
+		}
+		else if (*x > aux)
+		{
+			if (map[*y][*x - 1] == '1')
+				brain->left = true;
+		}
+		else if (*x == 0 && map[*y - 1][*x] == '1')
+			brain->up = true;
+		else if (map[*y][*x + 1] == '1')
+			brain->right = true;
+	}
+	else if (len < aux && *x == 0 && map[*y - 1][*x] == '1')
+		brain->up = true;
 }
